8960dd964678113b5d2ead436a4a436750934a08
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-09-23 17:17:13 -0300

    Implemented Target Logic
     - Created separated prefabs for each projectile for convenience

---
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] TargetCode target;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Target hitObj = collision.gameObject.GetComponentInParent<Target>();
+        if (hitObj && hitObj.code == target)
+        {
+            Destroy(hitObj.gameObject);
+            Debug.Log("Hit: " + collision.transform.name);
+        }
+    }
+}
--- Assets/Scripts/Towers/TowerShooter.cs
+++ Assets/Scripts/Towers/TowerShooter.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class TowerShooter : MonoBehaviour
 {
-    [SerializeField] TargetCode target;
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] float firePower = 50f;
 
@@ -23,7 +22,6 @@ public class TowerShooter : MonoBehaviour
         if (!towerController.IsActivated) return;
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log(gameObject.name + "shot something at " + target);
             GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
             Rigidbody projectileBody = projectile.GetComponent<Rigidbody>();
             projectileBody.AddForce(transform.forward * firePower, ForceMode.Impulse);
