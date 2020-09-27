3b675fdd50b4639caad3ff59ca4c0563ab435288
Caio Costa <caiocrrodrigues2004@gmail.com>
2020-09-27 00:42:31 -0300

    (2) Added how to play panel

---
+++ Assets/Scripts/UI/How2Play.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class How2Play : MonoBehaviour
+{
+    public GameObject menuObject;
+    public GameObject how2PlayObject;
+
+    public void showHow2Play()
+    {
+        menuObject.SetActive(false);
+        how2PlayObject.SetActive(true);
+    }
+
+    public void exitHot2Play()
+    {
+        how2PlayObject.SetActive(false);
+        menuObject.SetActive(true);
+    }
+}
